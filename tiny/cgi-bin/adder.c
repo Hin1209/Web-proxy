095254077448a298dc98d4ecdfc70f816d37a818
Hin1209 <hyeongin2024@gmail.com>
2023-04-15 15:09:32 +0000

    Feat: [#1] tiny server 구현

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,37 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void)
+{
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+  char *method = getenv("REQUEST_METHOD");
+
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p + 1);
+    p = strchr(arg1, '=');
+    n1 = atoi(p + 1);
+    p = strchr(arg2, '=');
+    n2 = atoi(p + 1);
+  }
+
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sThe Internet addition portal. \r\n<p>", content);
+  sprintf(content, "%sThe answer is :%d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  if (!strcasecmp(method, "GET"))
+    printf("%s", content);
+  fflush(stdout);
   exit(0);
 }
 /* $end adder */
