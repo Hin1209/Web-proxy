d4e87366ca0248652ef92ed3dbd90d54f46ec042
Hin1209 <hyeongin2024@gmail.com>
2023-04-17 10:51:30 +0000

    Refactor: transfer request, transfer response, parse uri 함수 추가

--- proxy.c
+++ proxy.c
@@ -10,7 +10,8 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-void doit(int fd);
+void *doit(void *vargp);
+void parse_uri(char *uri, char *host, char *port, char **path);
 
 int main(int argc, char **argv)
 {
@@ -42,55 +43,82 @@ int main(int argc, char **argv)
 
 void doit(int fd)
 {
-  int proxyfd, filesize, tmp, n;
-  char buf[MAXLINE], bbuf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], host[MAXLINE], port[MAXLINE];
-  char *p, *srcp, *readp, *portp, *pathp, *hostp;
+  int proxyfd, fd;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], host[MAXLINE], port[MAXLINE];
+  char *srcp, *path;
   rio_t rio, rio_client;
 
   Rio_readinitb(&rio_client, fd);
   Rio_readlineb(&rio_client, buf, MAXLINE);
+
   sscanf(buf, "%s %s %s", method, uri, version);
-  portp = strchr(uri + 5, ':');
-  pathp = strchr(portp, '/');
-  int idx = 0;
-  for (char *i = portp + 1; i < pathp; i++)
-    port[idx++] = *i;
-  hostp = strchr(uri, '/');
-  hostp = hostp + 2;
-  idx = 0;
-  for (char *i = hostp; i < portp; i++)
-    host[idx++] = *i;
+  parse_uri(uri, host, port, &path);
 
   proxyfd = Open_clientfd(host, port);
   Rio_readinitb(&rio, proxyfd);
-  sprintf(bbuf, "%s %s HTTP/1.0\r\n", method, pathp);
+  transfer_request(&rio_client, proxyfd, method, path, buf);
+  transfer_response(&rio, fd, buf);
+
+  free(srcp);
+  Close(proxyfd);
+  Close(fd);
+}
+
+void parse_uri(char *uri, char *host, char *port, char **path)
+{
+  char *portp, *hostp;
+  int idx = 0;
+
+  portp = strchr(uri + 5, ':');
+  *path = strchr(portp, '/');
+
+  for (char *i = portp + 1; i < *path; i++)
+    port[idx++] = *i;
+
+  hostp = strchr(uri, '/');
+  hostp = hostp + 2;
+  idx = 0;
+
+  for (char *i = hostp; i < portp; i++)
+    host[idx++] = *i;
+}
+
+void transfer_request(rio_t *rio_client, int proxyfd, char *method, char *path, char *buf)
+{
+  char buf_cat[MAXLINE];
+  sprintf(buf_cat, "%s %s HTTP/1.0\r\n", method, path);
   while (strcmp(buf, "\r\n"))
   {
-    Rio_readlineb(&rio_client, buf, MAXLINE);
+    Rio_readlineb(rio_client, buf, MAXLINE);
     if (strstr(buf, "User-Agent"))
-      sprintf(bbuf, "%s%s", bbuf, user_agent_hdr);
+      sprintf(buf_cat, "%s%s", buf_cat, user_agent_hdr);
     else
-      sprintf(bbuf, "%s%s", bbuf, buf);
+      sprintf(buf_cat, "%s%s", buf_cat, buf);
   }
-  Rio_writen(proxyfd, bbuf, strlen(bbuf));
-  Rio_readlineb(&rio, buf, MAXLINE);
-  sprintf(bbuf, "");
+  Rio_writen(proxyfd, buf_cat, strlen(buf_cat));
+}
+
+void transfer_response(rio_t *rio, int fd, char *buf)
+{
+  int filesize;
+  char buf_cat[MAXLINE], *p, *srcp;
+
+  Rio_readlineb(rio, buf, MAXLINE);
+  sprintf(buf_cat, "");
   while (strcmp(buf, "\r\n"))
   {
-    sprintf(bbuf, "%s%s", bbuf, buf);
+    sprintf(buf_cat, "%s%s", buf_cat, buf);
     if (strstr(buf, "Content-length"))
     {
       p = strchr(buf, ':');
       filesize = atoi(p + 1);
     }
-    Rio_readlineb(&rio, buf, MAXLINE);
+    Rio_readlineb(rio, buf, MAXLINE);
   }
-  sprintf(bbuf, "%s\r\n", bbuf);
-  Rio_writen(fd, bbuf, strlen(bbuf));
-  sprintf(bbuf, "");
+  sprintf(buf_cat, "%s\r\n", buf_cat);
+  Rio_writen(fd, buf_cat, strlen(buf_cat));
+  sprintf(buf_cat, "");
   srcp = Malloc(filesize);
-  Rio_readnb(&rio, srcp, filesize);
+  Rio_readnb(rio, srcp, filesize);
   Rio_writen(fd, srcp, filesize);
-  free(srcp);
-  Close(proxyfd);
 }
